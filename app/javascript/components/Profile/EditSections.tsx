8d3fa8e861c6a3616629df366d71ce9c8b20279d
Sarthak Garg <gr3y.m47t3r@gmail.com>
2025-10-14 08:56:40 +0530

    refactor: add padding for large screen

--- app/javascript/components/Profile/EditSections.tsx
+++ app/javascript/components/Profile/EditSections.tsx
@@ -498,7 +498,7 @@ const RichTextSectionView = ({ section }: { section: RichTextSection }) => {
           <RichTextEditorToolbar editor={editor} productId={section.id} />
         </div>
       ) : null}
-      <EditorContent editor={editor} className="rich-text p-1" />
+      <EditorContent editor={editor} className="rich-text lg:p-1" />
     </SectionLayout>
   );
 };
